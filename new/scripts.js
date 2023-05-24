4bcf454ad34c0f4d1f45c4792ef87a44036ac5b6
Kaveesh Khattar <kaveeshkhattar@gmail.com>
2023-05-24 16:20:31 +0530

    Summer New Website

---
+++ new/scripts.js
@@ -0,0 +1,40 @@
+function switchTheme(){
+    document.body.classList.toggle("dark-mode");
+    
+    var button = document.getElementById("darkModeButton");
+    var image = button.getElementsByTagName("img")[0];
+    
+    if (image.src.endsWith("moon.png")) {
+      image.src = "imgs/sun.png";
+    } else {
+      image.src = "imgs/moon.png";
+    }
+
+    var imageElement = document.getElementById("github");  
+    if (imageElement.src.includes("imgs/github.png")) {
+        imageElement.src = "imgs/github-dark-mode.png";
+        imageElement.alt = "Dark Image";
+    } else {
+        imageElement.src = "imgs/github.png";
+        imageElement.alt = "Light Image";
+    }
+    
+    var imageElement = document.getElementById("github-mobile");  
+    if (imageElement.src.includes("imgs/github.png")) {
+        imageElement.src = "imgs/github-dark-mode.png";
+        imageElement.alt = "Dark Image";
+    } else {
+        imageElement.src = "imgs/github.png";
+        imageElement.alt = "Light Image";
+    }
+}
+
+function openPDF() {
+    var pdfURL = "pdfs/certificate.jpg";
+    window.open(pdfURL, "_blank");
+  }
+
+  function openResume() {
+    var pdfURL = "pdfs/Resume.pdf";
+    window.open(pdfURL, "_blank");
+  }
